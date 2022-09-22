7bf633ecfbc0320f0fb87814b5057e253b03108b
koushik <koushik.dey@live.com>
2022-09-22 23:00:19 +0300

    team admin customization

--- pages/admin.py
+++ pages/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Team
+from django.utils.html import format_html
 
 # Register your models here.
 
-admin.site.register(Team)
-    
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius: 10px;" />'.format(object.photo.url))
+
+    thumbnail.short_description = 'Photo'
+    list_display = ('id', 'thumbnail', 'first_name', 'last_name', 'designation', 'created_date')
+    list_display_links = ('id','first_name',)
+
+admin.site.register(Team, TeamAdmin)
+
