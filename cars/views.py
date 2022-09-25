1216ed14627a7e0175eeff141b0ba807751db2c3
koushik <koushik.dey@live.com>
2022-09-26 01:12:54 +0300

    single car page url setup

--- cars/views.py
+++ cars/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def cars(request):
     return render(request, 'cars/cars.html')
+
+def car_detail(request, id):
+    return render(request, 'cars/car_detail.html')
