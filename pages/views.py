0236bb552f0c10d032eb58ebf5020f5e7ed07dbc
koushik <koushik.dey@live.com>
2022-09-25 19:59:43 +0300

    added dynamic featured cars

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import Team
+from cars.models import Car
 
 # Create your views here.
 
 def home(request):
     teams = Team.objects.all()
+    featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
     data = {
         'teams': teams,
+        'featured_cars': featured_cars,
     }
     return render(request, 'pages/home.html', data)
 
