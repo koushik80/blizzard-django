1812ffdf73431d6bd9e576168d6425e88ff96310
koushik <koushik.dey@live.com>
2022-09-26 13:04:09 +0300

    single car data conncted with db

--- cars/views.py
+++ cars/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Car
 
 # Create your views here.
 
@@ -6,4 +7,9 @@ def cars(request):
     return render(request, 'cars/cars.html')
 
 def car_detail(request, id):
-    return render(request, 'cars/car_detail.html')
+    single_car = get_object_or_404(Car, pk=id)
+
+    data = {
+        'single_car': single_car,
+    }
+    return render(request, 'cars/car_detail.html', data)
