5ed9e5ec593212d91d107cbe65ba1e700d76062f
koushik <koushik.dey@live.com>
2022-09-30 00:20:18 +0300

    :white_check_mark: login functionality successful
    d

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
-#from contacts.models import Contact
-from django.contrib.auth.decorators import login_required
-
 
 
 # Create your views here.
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
 def register(request):
