4e6452c01a28e16830d7d2b2bcdbc9e4ab32dc36
koushik <koushik.dey@live.com>
2022-09-29 14:22:49 +0300

    login, register & dashboard pages setup

--- blizzard/settings.py
+++ blizzard/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'cars.apps.CarsConfig',
     'pages.apps.PagesConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
