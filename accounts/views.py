da5d1c30c098927d3ef3251a3530b96709b8e900
koushik <koushik.dey@live.com>
2022-09-29 23:53:26 +0300

    :white_check_mark: user registration successful!
    d

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+#from contacts.models import Contact
+from django.contrib.auth.decorators import login_required
+
+
 
 # Create your views here.
 
@@ -8,8 +13,33 @@ def login(request):
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'This is error message')
-        return redirect('register')
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already exists!')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'Email already exists!')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(first_name=firstname, last_name=lastname, email=email, username=username, password=password)
+                    auth.login(request, user)
+                    messages.success(request, 'You are now logged in.')
+                    return redirect('dashboard')
+                    user.save()
+                    messages.success(request, 'You are registered successfully.')
+                    return redirect('login')
+
+        else:
+            messages.error(request, 'Password do not match')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
