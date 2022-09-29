11bcbc40e29639cd24464cc0b943b370f866bdb9
koushik <koushik.dey@live.com>
2022-09-30 01:50:13 +0300

    :white_check_mark: logout functionality successful!
    d

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -53,8 +54,13 @@ def register(request):
     else:
         return render(request, 'accounts/register.html')
 
+
+@login_required(login_url = 'login')
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
 
 def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
     return redirect('home')
