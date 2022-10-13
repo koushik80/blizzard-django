47a70f2d5ba10edda156f14419d5c437bf05126e
koushik <koushik.dey@live.com>
2022-10-13 21:34:34 +0300

    contact test

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
-
+from django.urls import reverse
 # Create your tests here.
+
+class ContactTests(TestCase):  # new
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/contact")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("contact"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("contact"))
+        self.assertTemplateUsed(response, "pages/contact.html")
+
+    def test_template_content(self):
+        response = self.client.get(reverse("contact"))
+        self.assertContains(response, "<h5>Email:</h5>")
+        self.assertNotContains(response, "Should not be here!")
