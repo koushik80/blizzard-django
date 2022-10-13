4af98f28b805e9ca70fdaa1ca0f790796b67865e
koushik <koushik.dey@live.com>
2022-10-13 21:05:33 +0300

    cars test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,43 +1,13 @@
 from django.test import TestCase
 from django.urls import reverse
 
+
 # Create your tests here.
 
 
-class DashboardPageTests(TestCase):  # new
-    def test_url_exists_at_correct_location(self):
-        response = self.client.get("/accounts/dashboard/")
-        self.assertEqual(response.status_code, 200)
-
-    def test_url_available_by_name(self):
-        response = self.client.get(reverse("dashboard"))
-        self.assertEqual(response.status_code, 200)
-
-    def test_template_name_correct(self):
-        response = self.client.get(reverse("dashboard"))
-        self.assertTemplateUsed(response, "accounts/dashboard.html")
-
-    def test_template_content(self):
-        response = self.client.get(reverse("dashboard"))
-        self.assertContains(response, "<p>Here are the cars that you have inquired about</p>")
-        self.assertNotContains(response, "Should not be here!")
 
 
 
-class RegisterPageTests(TestCase):  # new
-    def test_url_exists_at_correct_location(self):
-        response = self.client.post("/accounts/register/")
-        self.assertEqual(response.status_code, 200)
 
-    def test_url_available_by_name(self):
-        response = self.client.post(reverse("register"))
-        self.assertEqual(response.status_code, 200)
 
-    def test_template_name_correct(self):
-        response = self.client.post(reverse("register"))
-        self.assertTemplateUsed(response, "accounts/register.html")
 
-    def test_template_content(self):
-        response = self.client.post(reverse("register"))
-        self.assertContains(response, "<span>Or Login With</span>")
-        self.assertNotContains(response, "Should not be here!")
--- cars/tests.py
+++ cars/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
-
+from django.urls import reverse
 # Create your tests here.
+
+class CarsTests(TestCase):  # new
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("cars"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("cars"))
+        self.assertTemplateUsed(response, "cars/cars.html")
+
+    def test_template_content(self):
+        response = self.client.get(reverse("cars"))
+        self.assertContains(response, "<h1>Our Car Inventory</h1>")
+        self.assertNotContains(response, "Should not be here!")
+
+
+class SearchTests(TestCase):  # new
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("search"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("search"))
+        self.assertTemplateUsed(response, "cars/search.html")
+
+    def test_template_content(self):
+        response = self.client.get(reverse("search"))
+        self.assertContains(response, "<h1>Search Results</h1>")
+        self.assertNotContains(response, "Should not be here!")
